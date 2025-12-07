cdf2db2d8db1ab017b8f8d29ab0eb9cb54a38c96
Jamsheera <1234jasihussain@gmail.com>
2025-12-07 09:12:55 +0530

    leetcode 605

---
+++ 605.js
@@ -0,0 +1,22 @@
+// var canPlaceFlowers = function (flowerbed, n) {
+//   let l = flowerbed.length;
+//   for (let i = 0; i < l; i++) {
+//     if (i == l - 1 && flowerbed[i] == 0 && flowerbed[i - 1] == 0) n--;
+//     else if (flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1]==0) {
+//       console.log(`i ${i} n ${n}`);
+
+//       n--;
+//       i++;
+//     }
+//   }
+//   return n <= 0;
+// };
+
+var canPlaceFlowers=(flowerbed,n)=>{
+for(let i=0;i<flowerbed.length&&n!==0;i++){
+    if(flowerbed[i]==0&&flowerbed[i-1]!==1)
+}
+}
+let flowerbed = [1, 0, 0, 0, 0, 0, 1];
+let n = 2;
+console.log(canPlaceFlowers(flowerbed, n));
