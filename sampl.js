d617b6a804febc46643f88ad70ba1fd4de26404a
Jamsheera <1234jasihussain@gmail.com>
2025-12-31 22:36:32 +0530

    leetcode 226 complted

--- sampl.js
+++ sampl.js
@@ -1,9 +1,11 @@
-async function getUser(){
-    let res=await fetch('https://jsonplaceholder.typicode.com/users/1')
-    
-    let users=await res.json()
-    console.log(users);
-    
-}
-getUser()
+const products = [
+  { id: 1, name: "Shirt", category: "Men", price: 1200, quantity: 2 },
+  { id: 2, name: "Dress", category: "Women", price: 2500, quantity: 1 },
+  { id: 3, name: "Jeans", category: "Men", price: 1800, quantity: 3 },
+  { id: 4, name: "Skirt", category: "Women", price: 1500, quantity: 2 },
+  { id: 5, name: "T-Shirt", category: "Men", price: 800, quantity: 5 }
+];
+
+let max=products.reduce((acc,p)=>acc+(p.quantity*p.price),0)
+console.log(max);
 
