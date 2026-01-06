e9588ba2658318a0de58c46fe32ea50014524f96
Jamsheera <1234jasihussain@gmail.com>
2026-01-06 14:06:44 +0530

    leetcode 88 completed

--- 14.js
+++ 14.js
@@ -1,4 +1,24 @@
-let num=6
-console.log(Math.sqrt(num));
+var merge = function(nums1, m, nums2, n) {
+    let right=n+m-1
+    let left=m
+    while(left>=0&&n-1>=0){
+        if(nums1[left]<nums2[n-1]){
+            nums1[right]=nums2[n-1]
+            n--
+           
+        }else{
+            nums1[right]=nums1[left]
+            left--
+        }
+        right--
+    }
+}
+
+let nums1=[0]
+let nums2=[1]
+let m=0,n=1
+
+merge(nums1,m,nums2,n)
+console.log(nums1);
 
 
